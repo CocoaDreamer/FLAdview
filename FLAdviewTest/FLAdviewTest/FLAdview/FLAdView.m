ae2327cbeb4cf100d31b7d6b3c461eaaf710bcf5
fengling2300 <fengling2300@163.com>
2015-05-09 10:55:58 +0800

    test

--- FLAdviewTest/FLAdviewTest/FLAdview/FLAdView.m
+++ FLAdviewTest/FLAdviewTest/FLAdview/FLAdView.m
@@ -3,7 +3,7 @@
 //  ImageTest
 //
 //  Created by apple on 15/5/5.
-//  Copyright (c) 2015年 HikVision. All rights reserved.
+//  Copyright (c) 2015年 fengling2300. All rights reserved.
 //
 
 #import "FLAdView.h"
--- FLAdviewTest/FLAdviewTest/FLAdview/TapImageView.m
+++ FLAdviewTest/FLAdviewTest/FLAdview/TapImageView.m
@@ -3,7 +3,7 @@
 //  ImageTest
 //
 //  Created by apple on 15-4-22.
-//  Copyright (c) 2015年 HikVision. All rights reserved.
+//  Copyright (c) 2015年 fengling2300. All rights reserved.
 //
 
 #import "TapImageView.h"
